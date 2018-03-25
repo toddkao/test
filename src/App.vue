4afa810d1683f0d9491717406f367e681c48faf3
LiquidWater <bluemoon_redsun@hotmail.com>
2018-03-25 12:20:46 -0400

    Copied primary layout of target page

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,97 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <b-navbar variant="dark" type="white" id="navbar" toggleable>
+      <b-navbar-brand href="#">
+        <img src="https://material.io/guidelines/static/spec/images/callouts/default.svg" width="50px" height="50px">
+        Test Site
+      </b-navbar-brand>
+      <b-navbar-nav>
+        <b-nav-item>LINK1</b-nav-item>
+        <b-nav-item>LINK2</b-nav-item>
+        <b-nav-item>LINK3</b-nav-item>
+        <b-nav-item>LINK4</b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
     <router-view/>
+    <footer>
+      <b-container class="footer-container">
+          <b-row>
+            <b-col>
+              <ul>
+                <li>Some company inc</li>
+                <li><img src="@/assets/logo.png" width="50px" height="50px"></li>
+              </ul>
+            </b-col>
+            <b-col>
+              <ul>
+                <li><a href="/">LINKS1</a></li>
+                <li><a href="/">LINKS2</a></li>
+                <li><a href="/">LINKS3</a></li>
+                <li><a href="/">LINKS4</a></li>
+              </ul>
+            </b-col>
+            <b-col>
+              <ul>
+                <li><a href="/">LINKS5</a></li>
+                <li><a href="/">LINKS6</a></li>
+                <li><a href="/">LINKS7</a></li>
+                <li><a href="/">LINKS8</a></li>
+              </ul>
+            </b-col>
+            <b-col>
+              <ul>
+                <li>SOCIAL MEDIA</li>
+              </ul>
+            </b-col>
+          </b-row>
+      </b-container>
+    </footer>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      msg: [1, 2, 3, 4, 5]
+    }
+  },
+  mounted () {},
+  methods: {
+    test () {
+      console.log('LOG')
+      console.log(
+        this.msg.map((x, i) => {
+          return x + 1
+        })
+      )
+    }
+  }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: auto
+}
+#navbar{
+  text-align: center;
+  margin: auto
+}
+footer{
+  background: #343a40;
+  color: antiquewhite
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  margin: 10px 10px;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,85 +1,110 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <!--h1>{{ msg }}</h1-->
+    <b-carousel id="carousel1"
+                style="text-shadow: 1px 1px 2px #333;"
+                controls
+                indicators
+                background="#ababab"
+                :interval="4000"
+                img-width="1024"
+                img-height="480"
+                v-model="slide"
+    >
+
+      <b-carousel-slide img-blank
+                        content-tag="div">
+        <div class="item">
+          <b-jumbotron header="Jumbotron" lead="Jumbotron in carousel test" >
+            <h5>Some info</h5>
+            <p>More info</p>
+            <b-btn variant="primary" href="#">A button</b-btn>
+          </b-jumbotron>
+          </div>
+      </b-carousel-slide>
+      <!-- Text slides with image -->
+      <b-carousel-slide caption="First slide"
+                        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+                        img-src="https://lorempixel.com/1024/480/technics/2/"
+      ></b-carousel-slide>
+
+      <!-- Slides with custom text -->
+      <b-carousel-slide img-src="https://lorempixel.com/1024/480/technics/4/">
+        <h1>Hello world!</h1>
+      </b-carousel-slide>
+
+      <!-- Slides with image only -->
+      <b-carousel-slide img-src="https://lorempixel.com/1024/480/technics/8/">
+      </b-carousel-slide>
+
+      <!-- Slides with img slot -->
+      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
+      <b-carousel-slide>
+        <img slot="img" class="d-block img-fluid w-100" width="1024" height="480"
+             src="https://lorempixel.com/1024/480/technics/5/" alt="image slot">
+      </b-carousel-slide>
+
+      <!-- Slide with blank fluid image to maintain slide aspect ratio -->
+      <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          eros felis, tincidunt a tincidunt eget, convallis vel est. Ut pellentesque
+          ut lacus vel interdum.
+        </p>
+      </b-carousel-slide>
+
+    </b-carousel>
+    <b-container class="news">
+      <b-row>
+        <b-col>
+          <h2>News</h2>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+            <b-card title="News Article 1"
+            img-src="https://lorempixel.com/600/300/food/5/"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 20rem;"
+            class="mb-2">
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the bulk of the card's content.
+            </p>
+            <b-button href="#" variant="primary">Go somewhere</b-button>
+            </b-card>
+        </b-col>
+        <b-col>
+          <b-card title="News Article 2"
+            img-src="https://lorempixel.com/600/300/food/5/"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 20rem;"
+            class="mb-2">
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the bulk of the card's content.
+            </p>
+            <b-button href="#" variant="primary">Go somewhere</b-button>
+            </b-card>
+        </b-col>
+        <b-col>
+          <b-card title="News Article 3"
+            img-src="https://lorempixel.com/600/300/food/5/"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 20rem;"
+            class="mb-2">
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the bulk of the card's content.
+            </p>
+            <b-button href="#" variant="primary">Go somewhere</b-button>
+            </b-card>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
@@ -96,7 +121,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
@@ -109,5 +135,14 @@ li {
 }
 a {
   color: #42b983;
+}
+#carousel1{
+  width: 1024px;
+  height: 480px;
+  margin: auto
+}
+#hello{
+  text-align: center;
+  margin: auto
 }
 </style>
